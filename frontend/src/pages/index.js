1847c8c0764e7cd80955a71c51857c760560a217
LewinJun <junxianalan@gmail.com>
2024-11-22 11:45:40 +0800

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import { utils } from "near-api-js";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useCallback, useRef } from "react";
 
 import Form from "@/components/Form";
 import SignIn from "@/components/SignIn";
@@ -12,21 +12,72 @@ import { GuestbookNearContract } from "@/config";
 export default function Home() {
   const { signedAccountId, wallet } = useContext(NearContext);
   const [messages, setMessages] = useState([]);
+  const [status, setStatus] = useState("normal")
+  const pageSize = 50
+
+  const getNextPageMessage = useCallback(async ()=> {
+    if (status !== 'normal') {
+      return
+    }
+    setStatus('loading')
+    try {
+      const total_messages = await wallet.viewMethod({
+        contractId: GuestbookNearContract,
+        method: "total_messages",
+      });
+      if (total_messages === messages.length) {
+        setStatus('normal')
+        return;
+      }
+      const offPage = total_messages - messages.length
+      const from_index = offPage;
+      const limit = `${from_index < pageSize ? from_index : pageSize}`
+      wallet.viewMethod({
+        contractId: GuestbookNearContract,
+        method: "get_messages",
+        args: { from_index: String(from_index), limit: limit },
+      }).then((msgs)=>{
+        const newMsgs = [...messages, ...msgs.reverse()];
+        setMessages(newMsgs)
+        setStatus('normal')
+      }).catch(e=>{
+        setStatus('normal')
+      });
+    } catch (error) {
+      setStatus('normal')
+    }
+  }, [messages, status])
 
   useEffect(() => {
-    getLast10Messages().then((messages) => setMessages(messages.reverse()));
+    getLast10Messages().then((messages) => {
+      setMessages(messages.reverse())
+    });
+    
   }, []);
+
+  useEffect(()=>{
+    const scrollBack = ()=> {
+      const currentMaxY = document.body.clientHeight - document.documentElement.clientHeight
+      if (window.scrollY >= currentMaxY - 20) {
+        getNextPageMessage()
+      }
+    }
+    window.addEventListener('scroll', scrollBack)
+    return ()=> {
+      window.removeEventListener('scroll', scrollBack)
+    }
+  }, [messages,status])
 
   const getLast10Messages = async () => {
     const total_messages = await wallet.viewMethod({
       contractId: GuestbookNearContract,
       method: "total_messages",
     });
-    const from_index = total_messages >= 10 ? total_messages - 10 : 0;
+    const from_index = total_messages >= pageSize ? total_messages - pageSize : 0;
     return wallet.viewMethod({
       contractId: GuestbookNearContract,
       method: "get_messages",
-      args: { from_index: String(from_index), limit: "10" },
+      args: { from_index: String(from_index), limit: `${pageSize}` },
     });
   };
 
@@ -66,8 +117,9 @@ export default function Home() {
           <SignIn />
         )}
       </div>
-
+      
       {!!messages.length && <Messages messages={messages} />}
+      {status === 'loading' && <div className={styles.loading}>Loading...</div>}
     </main>
   );
 }
